d10cbcbec68270de8166f68b4adfead41de60c8d
Dragos Silion <silidragos@gmail.com>
2022-10-30 16:30:51 +0200

    Cerinta

--- src/courses/day4-texturing.tsx
+++ src/courses/day4-texturing.tsx
@@ -1,3 +1,22 @@
+/*
+Concepts:
+    - Basic materials
+    - Physical materials - roughness, metallic
+    - Activating Transparency
+Bonus:
+    - Using maps
+    - Normal maps
+Tasks:
+    We need the following materials
+    - Basic red
+    - Basic blue opacity 50%
+    - Physical: White, full metallic, smooth
+    - Physical: green, smooth
+    - Physical: White, full metallic, smooth, 50% transparency
+Hints:
+    - In case transparency doesn't work...
+ */
+
 export default function Day4_Texturing() {
 
     return ([
