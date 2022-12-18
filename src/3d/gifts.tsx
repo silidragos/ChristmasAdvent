3bdbc31afb97df491a302b585908787fd26ecb65
Alexandru Pavaloi <a.pavaloi@gmail.com>
2022-12-18 12:29:19 +0200

    [COSMETIC] - gifts.tsx

--- src/3d/gifts.tsx
+++ src/3d/gifts.tsx
@@ -1,23 +1,21 @@
-import { useEffect, useMemo, useRef } from "react";
-import { Vector3, BufferGeometry, PositionalAudio } from "three";
 import * as THREE from "three";
+import { useMemo } from "react";
+import { extend, ReactThreeFiber } from "@react-three/fiber";
+import { Vector3, BufferGeometry, PositionalAudio } from "three";
 
-import GiftFactory from "../services/ElementsFactory";
 import Curve from "./curve";
-
-import Day2_CustomShapes from "../courses/day2-custom-shapes";
-import { Day3_useFrame } from "../courses/day3-useFrame";
-import { Line } from "@react-three/drei";
-import { extend, ReactThreeFiber, useLoader } from "@react-three/fiber";
-import AudioComponent, { listener, TryPlaySound } from "./audio-component";
-import {
-    Test1Component,
-    Test2,
-    Test2Component,
-    Test2Passed,
-    Test4Component,
-} from "../services/TestingService";
+import GiftFactory from "../services/ElementsFactory";
 import { AUDIO_PUBLIC_URL } from "../services/Constants";
+import { Day3_useFrame } from "../courses/day3-useFrame";
+import Day2_CustomShapes from "../courses/day2-custom-shapes";
+import AudioComponent, { TryPlaySound } from "./audio-component";
+import {
+  Test1Component,
+  Test2,
+  Test2Component,
+  Test2Passed,
+  Test4Component,
+} from "../services/TestingService";
 
 const giftSpeed = 0.05;
 
@@ -25,77 +23,77 @@ const giftSpeed = 0.05;
 extend({ Line_: THREE.Line })
 
 declare global {
-    namespace JSX {
-        interface IntrinsicElements {
-            line_: ReactThreeFiber.Object3DNode<THREE.Line, typeof THREE.Line>
-        }
+  namespace JSX {
+    interface IntrinsicElements {
+      line_: ReactThreeFiber.Object3DNode<THREE.Line, typeof THREE.Line>
     }
+  }
 }
 
 export default function Gifts() {
-    let giftsSound: PositionalAudio;
+  let giftsSound: PositionalAudio;
 
-    let [myLineGeometry, curve] = useMemo(() => {
-        let points = [
-            new Vector3(0, 0, 0),
-            new Vector3(-0.8, 0, -.05),
-            new Vector3(-1.1, 0, -.25),
-            new Vector3(-1.3, 0, -.7),
-            new Vector3(-1.1, 0, -1.3),
-            new Vector3(-0.4, 0, -1.8),
-            new Vector3(0.5, 0, -2.1),
-            new Vector3(1.4, 0, -2.1),
-            new Vector3(2.5, 0, -2.1),
-            new Vector3(3.5, 0, -1.6),
-            new Vector3(4.1, 0, -1.0),
-            new Vector3(4.2, 0, -.75),
-            new Vector3(4.3, 0, 0.5),
-            new Vector3(4.0, 0, 1.3),
-            new Vector3(3.5, 0, 1.7),
-            new Vector3(2.5, 0, 1.85),
-            new Vector3(1.5, 0, 2)];
+  let [myLineGeometry, curve] = useMemo(() => {
+    let points = [
+      new Vector3(0, 0, 0),
+      new Vector3(-0.8, 0, -.05),
+      new Vector3(-1.1, 0, -.25),
+      new Vector3(-1.3, 0, -.7),
+      new Vector3(-1.1, 0, -1.3),
+      new Vector3(-0.4, 0, -1.8),
+      new Vector3(0.5, 0, -2.1),
+      new Vector3(1.4, 0, -2.1),
+      new Vector3(2.5, 0, -2.1),
+      new Vector3(3.5, 0, -1.6),
+      new Vector3(4.1, 0, -1.0),
+      new Vector3(4.2, 0, -.75),
+      new Vector3(4.3, 0, 0.5),
+      new Vector3(4.0, 0, 1.3),
+      new Vector3(3.5, 0, 1.7),
+      new Vector3(2.5, 0, 1.85),
+      new Vector3(1.5, 0, 2)];
 
-        const lineGeometry = new BufferGeometry().setFromPoints(points);
-        return [lineGeometry, new Curve(points)];
-    }, []);
+    const lineGeometry = new BufferGeometry().setFromPoints(points);
+    return [lineGeometry, new Curve(points)];
+  }, []);
 
-    let giftFactory: GiftFactory = useMemo(() => {
-        return new GiftFactory(Day2_CustomShapes);
-    }, []);
+  let giftFactory: GiftFactory = useMemo(() => {
+    return new GiftFactory(Day2_CustomShapes);
+  }, []);
 
-    const getGifts = function () {
-        let gifts: any[] = [];
-        let count = 10;
-        Test2(giftFactory.getAll());
+  const getGifts = function () {
+    let gifts: any[] = [];
+    let count = 10;
+    Test2(giftFactory.getAll());
 
-        if (Test2Passed()) {
-            for (let i = 0; i < count; i++) {
-                gifts.push(
-                    <Day3_useFrame key={i} curve={curve} offset={i * (1.0 / count)} giftSpeed={giftSpeed} onRespawn={() => {
-                        TryPlaySound(giftsSound);
-                    }}>
-                        {giftFactory.getRandom()}
-                    </Day3_useFrame >
-                );
-            }
-        }
-        return gifts;
+    if (Test2Passed()) {
+      for (let i = 0; i < count; i++) {
+        gifts.push(
+          <Day3_useFrame key={i} curve={curve} offset={i * (1.0 / count)} giftSpeed={giftSpeed} onRespawn={() => {
+            TryPlaySound(giftsSound);
+          }}>
+            {giftFactory.getRandom()}
+          </Day3_useFrame >
+        );
+      }
     }
-    return (
-        <>
-            <group position={[-1.5, 1, 0]}>
-                <line_ geometry={myLineGeometry}>
-                    <lineBasicMaterial attach="material" color="red"></lineBasicMaterial>
-                </line_>
-                {getGifts()}
+    return gifts;
+  }
+  return (
+    <>
+      <group position={[-1.5, 1, 0]}>
+        <line_ geometry={myLineGeometry}>
+          <lineBasicMaterial attach="material" color="red"></lineBasicMaterial>
+        </line_>
+        {getGifts()}
 
-                <AudioComponent url={`${AUDIO_PUBLIC_URL}/boop.mp3`} volume={1} loop={false} autoplay={false} play={false} onInit={sound => {
-                    giftsSound = sound;
-                }} />
-            </group>
-            <Test1Component />
-            <Test2Component giftFactory={giftFactory} />
-            <Test4Component />
-        </>
-    );
+        <AudioComponent url={`${AUDIO_PUBLIC_URL}/boop.mp3`} volume={1} loop={false} autoplay={false} play={false} onInit={sound => {
+          giftsSound = sound;
+        }} />
+      </group>
+      <Test1Component />
+      <Test2Component giftFactory={giftFactory} />
+      <Test4Component />
+    </>
+  );
 }
