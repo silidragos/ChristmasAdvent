cbe2f697ec9ac9e45451fdc86b9b2b427b1bf4f4
Alexandru Pavaloi <a.pavaloi@gmail.com>
2022-12-19 19:52:27 +0200

    [COSMETIC] - Use 2 space indentation

--- src/3d/energy-cube.tsx
+++ src/3d/energy-cube.tsx
@@ -7,52 +7,52 @@ import { Test1, Test1Component } from "../services/TestingService";
 import HierarchyDay1, { MESH_NAMES } from "../courses/day1-hierarchy-attributes";
 
 interface Props {
-    nodes: Nodes;
-    materials: Materials;
-    onTestChange: (valid: boolean) => void;
+  nodes: Nodes;
+  materials: Materials;
+  onTestChange: (valid: boolean) => void;
 }
 
 export default function EnergyCube({
-    nodes,
-    materials,
-    onTestChange,
+  nodes,
+  materials,
+  onTestChange,
 }: Props) {
-    const { scene } = useThree();
-    const batteryCore = useRef<Mesh>(null);
-    const batteryCoreMainPosition = useRef<Vector3 | null>(null);
+  const { scene } = useThree();
+  const batteryCore = useRef<Mesh>(null);
+  const batteryCoreMainPosition = useRef<Vector3 | null>(null);
 
-    useEffect(() => {
-        if (batteryCore.current === null) {
-            return;
-        }
+  useEffect(() => {
+    if (batteryCore.current === null) {
+      return;
+    }
 
-        batteryCoreMainPosition.current = batteryCore.current.position.clone();
-    }, [batteryCore.current]);
+    batteryCoreMainPosition.current = batteryCore.current.position.clone();
+  }, []);
 
-    useFrame(() => {
-        if (batteryCore.current === null || batteryCoreMainPosition.current === null) {
-            return;
-        }
+  useFrame(() => {
+    if (batteryCore.current === null || batteryCoreMainPosition.current === null) {
+      return;
+    }
 
-        let testResult = Test1(scene, MESH_NAMES.LEFT_BATTERY, MESH_NAMES.RIGHT_BATTERY);
+    let testResult = Test1(scene, MESH_NAMES.LEFT_BATTERY, MESH_NAMES.RIGHT_BATTERY);
 
-        if (testResult.valid) {
-            let newPos = new Vector3();
-            newPos.addVectors(new Vector3(Math.random(), Math.random(), Math.random()).multiplyScalar(0.2), batteryCoreMainPosition.current);
-            batteryCore.current.position.set(newPos.x, newPos.y, newPos.z);
-        }
-        onTestChange(testResult.valid);
-    });
-    return (
-        <>
-            <group>
-                <HierarchyDay1 nodes={nodes} materials={materials} />
-                <group position={[-0.74, 2.86, 0.07]} scale={0.4}>
-                    <mesh ref={batteryCore} geometry={nodes.EnergySphere_1.geometry} material={materials.Red} />
-                    <mesh geometry={nodes.EnergySphere_2.geometry} material={materials.Glass} />
-                </group>
-            </group>
-            <Test1Component leftBatteryName={MESH_NAMES.LEFT_BATTERY} rightBatteryName={MESH_NAMES.RIGHT_BATTERY} />
-        </>
-    );
+    if (testResult.valid) {
+      let newPos = new Vector3();
+      newPos.addVectors(new Vector3(Math.random(), Math.random(), Math.random()).multiplyScalar(0.2), batteryCoreMainPosition.current);
+      batteryCore.current.position.set(newPos.x, newPos.y, newPos.z);
+    }
+    onTestChange(testResult.valid);
+  });
+  return (
+    <>
+      <group>
+        <HierarchyDay1 nodes={nodes} materials={materials} />
+        <group position={[-0.74, 2.86, 0.07]} scale={0.4}>
+          <mesh ref={batteryCore} geometry={nodes.EnergySphere_1.geometry} material={materials.Red} />
+          <mesh geometry={nodes.EnergySphere_2.geometry} material={materials.Glass} />
+        </group>
+      </group>
+      <Test1Component leftBatteryName={MESH_NAMES.LEFT_BATTERY} rightBatteryName={MESH_NAMES.RIGHT_BATTERY} />
+    </>
+  );
 }
