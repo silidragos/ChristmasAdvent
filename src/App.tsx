95edd11e1868a77842c5ea807de769d21dd88726
Alexandru Pavaloi <a.pavaloi@gmail.com>
2022-12-21 18:05:25 +0200

    [COSMETIC] - ESLint and remove useless component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import { Factory } from './3d/factory';
-import { Suspense, useEffect, useState } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber'
-import { Debug, Physics } from '@react-three/cannon';
-import { WINDOW_EVENTS } from './services/Constants';
+import { Suspense, useState } from 'react';
+import { Canvas } from '@react-three/fiber'
+import { Physics } from '@react-three/cannon';
 import { OrbitControls, Environment } from '@react-three/drei';
-import { emitWindowEvent, useWindowEvent } from './services/WindowEvents';
+
+import { Factory } from './3d/factory';
 
 import './App.css';
 
-
-
-function MuteUnmute(){
+function MuteUnmute() {
   const [isMute, setIsMute] = useState(true);
 
-  return(
+  return (
     <div>
       <button className="mute-but" onClick={() => {
-                        document.dispatchEvent(new CustomEvent("muteStateUpdated", { detail: { isMute: !isMute } }));
-                        setIsMute(!isMute);
-                    }}><span>{isMute ? "unmute": "mute"}</span></button>
+        document.dispatchEvent(new CustomEvent("muteStateUpdated", { detail: { isMute: !isMute } }));
+        setIsMute(!isMute);
+      }}><span>{isMute ? "unmute" : "mute"}</span></button>
     </div>
   )
 }
@@ -26,51 +23,18 @@ function MuteUnmute(){
 export default function App() {
   return (
     <div id="canvas-container">
-      <MuteUnmute/>
+      <MuteUnmute />
       <Canvas>
         <Suspense>
-        <OrbitControls></OrbitControls>
-        <directionalLight position={[0, 2, -2]} color={"white"} intensity={.5} />
-        <ambientLight intensity={.1}/>
-        <Environment background={false} near={1} far={20} resolution={256} preset="apartment"></Environment>
-        <Physics gravity={[0, -1.98, 0]}>
-          <Debug color="white" scale={1.01}>
+          <OrbitControls></OrbitControls>
+          <directionalLight position={[0, 2, -2]} color={"white"} intensity={.5} />
+          <ambientLight intensity={.1} />
+          <Environment background={false} near={1} far={20} resolution={256} preset="apartment"></Environment>
+          <Physics gravity={[0, -1.98, 0]}>
             <Factory />
-          </Debug>
-        </Physics>
-        <LoadListener />
+          </Physics>
         </Suspense>
       </Canvas>
     </div>
   )
-}
-
-// Custom component that
-// > 1. emits a custom event on the Window when the Scene has loaded
-// > 2. listens to a custom event that intends to check if the scene
-// has loaded and responds with 
-const LoadListener = () => {
-  const [didLoad, setDidLoad] = useState(false);
-
-  // FIXME
-  // Current implementation is "fake" and doesn't
-  // actually check if the scene is loaded.
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setDidLoad(true);
-    }, 5000);
-
-    return () => {
-      clearTimeout(timeoutId);
-    }
-  }, []);
-
-  useWindowEvent(WINDOW_EVENTS.TEST_LOAD, () => {
-    emitWindowEvent({
-      type: WINDOW_EVENTS.TEST_LOAD_RESULT,
-      payload: true,
-    });
-  }, [didLoad]);
-
-  return null;
 }
