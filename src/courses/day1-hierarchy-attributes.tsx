d14c547b277f175b75d75f46903895e79d4fd1a8
Alexandru Pavaloi <a.pavaloi@gmail.com>
2022-12-21 17:46:49 +0200

    [COSMETIC] - day1

--- src/courses/day1-hierarchy-attributes.tsx
+++ src/courses/day1-hierarchy-attributes.tsx
@@ -1,28 +1,26 @@
-import React, { useRef } from "react"
 import { Materials, Nodes } from "../3d/3d.types"
 
-
 export const MESH_NAMES = {
-    LEFT_BATTERY: "left-battery-mesh",
-    RIGHT_BATTERY: "right-battery-mesh"
+  LEFT_BATTERY: "left-battery-mesh",
+  RIGHT_BATTERY: "right-battery-mesh"
 };
 
 export default function Day1_HierarchyAndAttributes({ nodes, materials }: { nodes: Nodes, materials: Materials }) {
-    return (
-        <>
-            <group position={[-2.5, 1, 0]} rotation={[0, 0, 0]}>
-            </group>
+  return (
+    <>
+      <group position={[-2.5, 1, 0]} rotation={[0, 0, 0]}>
+      </group>
 
-            <group position={[-2.5, 1, -.5]} rotation={[-Math.PI/6, Math.PI/6.0, 0]}>
-            </group>
+      <group position={[-2.5, 1, -.5]} rotation={[-Math.PI / 6, Math.PI / 6.0, 0]}>
+      </group>
 
-            <group name="left-battery-holder" position={[-.75, 2.1, .8]} rotation={[Math.PI/2, 0 , 0]}>
-                <mesh  name={MESH_NAMES.LEFT_BATTERY} geometry={nodes.Battery.geometry} material={materials.Battery}/>
-            </group>
-            
-            <group name="right-battery-holder" position={[-.75, 2.1, -.65]}  rotation={[-Math.PI/2, 0 , 0]}>
-                <mesh name={MESH_NAMES.RIGHT_BATTERY} geometry={nodes.Battery.geometry} material={materials.Battery}/>
-            </group>
-        </>
-    )
+      <group name="left-battery-holder" position={[-.75, 2.1, .8]} rotation={[Math.PI / 2, 0, 0]}>
+        <mesh name={MESH_NAMES.LEFT_BATTERY} geometry={nodes.Battery.geometry} material={materials.Battery} />
+      </group>
+
+      <group name="right-battery-holder" position={[-.75, 2.1, -.65]} rotation={[-Math.PI / 2, 0, 0]}>
+        <mesh name={MESH_NAMES.RIGHT_BATTERY} geometry={nodes.Battery.geometry} material={materials.Battery} />
+      </group>
+    </>
+  )
 }
